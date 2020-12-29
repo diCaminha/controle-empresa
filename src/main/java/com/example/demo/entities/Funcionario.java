e4939e6f4b2569525e901f83d7d0c18bd43f17b2
Denis <dineresc@gmail.com>
2020-12-29 00:25:40 -0300

    creating class funcionario

--- src/main/java/com/example/demo/entities/Funcionario.java
+++ src/main/java/com/example/demo/entities/Funcionario.java
@@ -1,4 +1,97 @@
 package com.example.demo.entities;
 
-public class Funcionario {
+import com.example.demo.enums.PerfilEnum;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Data
+@Table(name = "funcionario")
+public class Funcionario implements Serializable {
+
+    public static final long serialVersionUID = -435345452452234L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    private String cpf;
+
+    @Column(name = "valor_hora", nullable = false)
+    private BigDecimal valorHora;
+
+    @Transient
+    public Optional<BigDecimal> getValorHoraOpt(){
+        return Optional.ofNullable(valorHora);
+    }
+
+    @Column(name = "qtd_horas_trabalho_dia")
+    private Float qtdHorasTrabalhoDia;
+
+    @Transient
+    public Optional<Float> getQtdHorasTrabalhoDiaOpt() {
+        return Optional.ofNullable(qtdHorasTrabalhoDia);
+    }
+
+    @Column(name = "qtd_horas_almoco")
+    private Float qtdHorasAlmoco;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "perfil")
+    private PerfilEnum perfil;
+
+    @Column(name = "data_criacao")
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    private Date dataAtualizacao;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Empresa empresa;
+
+    @OneToMany(mappedBy = "funcionario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Lancamento> lancamentos;
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", valorHora=" + valorHora +
+                ", qtdHorasTrabalhoDia=" + qtdHorasTrabalhoDia +
+                ", qtdHorasAlmoco=" + qtdHorasAlmoco +
+                ", perfil=" + perfil +
+                ", dataCriacao=" + dataCriacao +
+                ", dataAtualizacao=" + dataAtualizacao +
+                ", empresa=" + empresa +
+                ", lancamentos=" + lancamentos +
+                '}';
+    }
 }
